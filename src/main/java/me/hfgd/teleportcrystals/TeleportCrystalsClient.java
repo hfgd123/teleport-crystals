ffc76ec082939b3b7301a55a2d9693c110a8970d
hfgd <jan.lunz@web.de>
2023-06-10 16:15:10 +0200

    Initial Commit

---
+++ src/main/java/me/hfgd/teleportcrystals/TeleportCrystalsClient.java
@@ -0,0 +1,10 @@
+package me.hfgd.teleportcrystals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TeleportCrystalsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/me/hfgd/teleportcrystals/TeleportCrystalsDataGenerator.java
@@ -0,0 +1,11 @@
+package me.hfgd.teleportcrystals;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TeleportCrystalsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
